aaa25e0be35b55d9d4bcd110d1ee7e4794bac74c
rashid <rassant@mail.ru>
2020-06-28 00:28:10 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <spdlog/spdlog.h>
+
+int main ()
+{
+    //spdlog::info("good day");
+     auto logger = spdlog::stdout_logger_mt("console");
+      logger->info("version {} was started", version());
+
+}
