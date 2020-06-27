f802d95221a1b1bc8c567d3ec42766b9dca44153
Rimkon <37692739+Rimkon@users.noreply.github.com>
2020-06-28 01:14:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <spdlog/spdlog.h>
-
+//#include <spdlog/spdlog.h>
+#include <iostream>
 int main ()
 {
     //spdlog::info("good day");
-     auto logger = spdlog::stdout_logger_mt("console");
-      logger->info("version {} was started", version());
-
+  //   auto logger = spdlog::stdout_logger_mt("console");
+    //  logger->info("version {} was started", version());
+std::cout<<"hello";
 }
